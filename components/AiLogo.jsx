792938eff6849b6f6388f40629746332a74b886a
Brilliant Makanjju <106150499+brilliantmakanju@users.noreply.github.com>
2023-03-15 17:49:59 -0400

    Update AiLogo.jsx

--- components/AiLogo.jsx
+++ components/AiLogo.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function AiLogo(){
     return(
-        <section className="lg:snap-always lg:snap-start bg-no-repeat bg-cover bg-center bg-fixed " style={{backgroundImage:"url(/AichatGPT.jpg)"}}  >
+        <section className="lg:snap-always lg:snap-start bg-no-repeat bg-cover bg-center bg-fixed "   >
             <div className="md:flex md:flex-col mx-auto gap-5 relative hidden h-[30em]" >
                 <div className="mx-auto relative w-[50em] h-[30em]  " >
                     <Image
