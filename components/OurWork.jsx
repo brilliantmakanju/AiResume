46578915cb47a0f29094d7074e3c50a1d771854e
Brilliant Makanjju <106150499+brilliantmakanju@users.noreply.github.com>
2023-03-15 17:30:18 -0400

    Update OurWork.jsx

--- components/OurWork.jsx
+++ components/OurWork.jsx
@@ -3,7 +3,7 @@ import OurWorkList from "./OurWorkList"
 export default function OurWork(){
     return(
         <section className="lg:snap-always lg:snap-start px-5 md:mx-auto md:py-[20px] lg:px-20 grid grid-cols-1 gap-10 md:flex-wrap py-[14%] md:py-[10%] ">
-            <h3 className="text-[25px] px-[5px] text-center font-bold font-serif tracking-wide h-[2em] md:h-[3em]  leading-tight text-center md:px-[5%] md:text-[40px]  lg:text-[45px] mx-auto lg:w-[100%] md:gap-10" >Create your Resume/Cover Letter or Proposal with our AI</h3>
+            <h3 className="text-[25px] px-[5px] text-center font-bold font-serif tracking-wide h-[2em] md:h-[3em] lg:px-[12%] leading-tight text-center md:px-[5%] md:text-[40px]  lg:text-[45px] mx-auto lg:w-[100%] md:gap-10" >Create your Resume/Cover Letter or Proposal with our AI</h3>
             <OurWorkList />
         </section>
     )
