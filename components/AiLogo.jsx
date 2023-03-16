55584843007b049fcab1685b8ade74849b0bc088
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-03-16 18:01:46 -0400

    Added the Login Page and Redesigned it

--- components/AiLogo.jsx
+++ components/AiLogo.jsx
@@ -2,7 +2,6 @@ import Image from "next/image"
 
 export default function AiLogo(){
     return(
-        <section className="bg-no-repeat bg-cover bg-center bg-fixed "   >
         <section className=" bg-no-repeat bg-cover bg-center bg-fixed "   >
             <div className="md:flex md:flex-col mx-auto gap-5 relative hidden h-[30em]" >
                 <div className="mx-auto relative w-[50em] h-[30em]  " >
