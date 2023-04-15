7fc17edc4ff3a500a574315d7df3b61aa7e9ccd0
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-04-15 13:05:46 +0100

    Changed the image ratio on desktop"

--- components/landing/HomeIntroImage.jsx
+++ components/landing/HomeIntroImage.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function HomeIntroImage() {
   return (
     <div className="lg:flex md:flex-col gap-5 py-10 md:py-20 relative h-[24em] lg:h-[39em] overflow-visible   ">
-      <div className="relative  lg:w-[cal(50% - 50vw)] xl:w-[cal(100% - 100vw)] w-[25em] h-[22em] lg:h-[55em]  mt-[-3em]  container mx-auto ">
+      <div className="relative  lg:w-[cal(50% - 50vw)] xl:w-[cal(100% - 100vw)] 2xl:w-[40em] w-[25em] h-[22em] lg:h-[55em]  mt-[-3em]  container mx-auto ">
         <Image
           fill
           alt="AiPic"
